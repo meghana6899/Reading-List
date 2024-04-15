717b7912213cd2a399d521eb17017bcbb4f377c5
unknown <vemulasaimeghana@gmail.com>
2024-04-16 01:15:19 +0530

    no data persistence

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,23 +1,25 @@
-import { useState, useContext } from "react";
-import BooksContext from "../context/books";
+import { useState } from 'react';
 
-function BookEdit({book, onSubmit}) {
-    const [title, setTitle] = useState(book.title)
-    const {editBookById} = useContext(BooksContext)
-    const handleChange = (event) => {
-        setTitle(event.target.value)
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onSubmit();
-        editBookById(book.id, title);
-    }
-    return <div>
-        <form className='book-edit' onSubmit={handleSubmit}>
-            <label>Title</label>
-            <input className='input' value={title} onChange={handleChange}/>
-            <button className="button is-primary">Save</button>
-        </form>
-    </div>
+function BookEdit({ book, onSubmit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(book.id, title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 }
+
 export default BookEdit;
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,45 +1,41 @@
-import { useState} from "react";
+import { useState } from 'react';
 import BookEdit from './BookEdit';
-import useBooksContext from "../hooks/use-books-context";
 
-function BookShow({book}) {
-    const {deleteBookById} = useBooksContext();
+function BookShow({ book, onDelete, onEdit }) {
+  const [showEdit, setShowEdit] = useState(false);
 
-    const handleDeleteClick=() => {
-       deleteBookById(book.id);
-    };
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
 
-    const [showEdit, setShowEdit] = useState(false);
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
 
-    const handleEditClick = () => {
-        setShowEdit(!showEdit);
-    };
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
 
-    const handleSubmit = () => {
-        setShowEdit(false);
-        
-    };
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
+  }
 
-    let content = <h3>{book.title}</h3>;
-    if (showEdit) {
-        content = <BookEdit onSubmit={handleSubmit} book={book} />;
-    }
-
-    return (
+  return (
     <div className="book-show">
-        <img alt="books" src = {`https://picsum.photos/seed/${book.id}/300/200` } />
-        <div>{content}</div>
-        <div className="actions">
-            <button className="edit" onClick={handleEditClick}>Edit</button>
-            <button className="delete" onClick={handleDeleteClick}>
-                Delete
-            </button>
-        </div>
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
     </div>
-    );
-    
-
-    }
-   
+  );
+}
 
 export default BookShow;
